271e73e59ab47178e3b6f2a3d966f310244d1168
Eran <agmon.eran@gmail.com>
2023-10-31 12:47:33 -0400

    initial commit

---
+++ api/builder.py
@@ -0,0 +1,23 @@
+from process_bigraph import Composite
+
+
+class Builder:
+
+    def __init__(self):
+        self.composite
+
+
+
+def test_builder():
+
+    # Initialize the Builder
+    sim = Builder()
+
+    sim.batch = {}
+
+
+
+
+
+if __name__ == '__main__':
+    test_builder()
