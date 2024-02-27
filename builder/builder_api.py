b272393480f5e2635d83895b283da7f59ac62a3e
Eran <agmon.eran@gmail.com>
2024-02-27 13:40:08 -0500

    work on adding process to with builder

--- builder/builder_api.py
+++ builder/builder_api.py
@@ -2,7 +2,9 @@ import os
 import json
 import pprint
 from bigraph_schema.registry import get_path, set_path
-from process_bigraph import ProcessTypes, Composite
+from bigraph_schema import Edge
+from bigraph_schema.protocols import local_lookup_module
+from process_bigraph import Process, Step, Composite, ProcessTypes
 from bigraph_viz.diagram import plot_bigraph
 
 
@@ -113,6 +115,35 @@ class BuilderNode:
     def top(self):
         return self.builder.node
 
+    def add_process(
+            self,
+            name,
+            config=None,
+            inputs=None,
+            outputs=None,
+            edge_type=None,
+            **kwargs
+    ):
+        """ Add a process to the tree """
+        # TODO -- assert this process is in the process_registry
+
+        assert name, 'add_process requires a name as input'
+        config = config or {}
+        config.update(kwargs)
+        edge_type = edge_type or 'process'  # TODO -- don't hardcode as process
+
+        # make the process spec
+        state = {
+            '_type': edge_type,
+            'address': f'local:{name}',  # TODO -- only support local right now?
+            'config': config,
+            'inputs': {} if inputs is None else inputs,
+            'outputs': {} if outputs is None else outputs,
+        }
+
+        set_path(tree=self.builder.tree, path=self.path, value=state)
+
+
 
 class Builder:
 
@@ -132,13 +163,18 @@ class Builder:
     def __repr__(self):
         return f"Builder({pf(self.tree)})"
 
-
     def __getitem__(self, keys):
         return self.node[keys]
 
     def __setitem__(self, keys, value):
         self.node.__setitem__(keys, value)
         self.complete()
+
+    def list_types(self):
+        return self.core.type_registry.list()
+
+    def list_processes(self):
+        print(self.core.process_registry.list())
 
     def complete(self):
         self.schema, self.tree = self.core.complete(self.schema, self.tree)
@@ -179,8 +215,45 @@ class Builder:
 
         print(f"File '{filename}' successfully written in '{outdir}' directory.")
 
+    def register_process(self, process_name, address=None):
+        """
+        Register processes into the local core type system
+        """
+        assert isinstance(process_name, str), f'Process name must be a string: {process_name}'
+
+        if address is None:  # use as a decorator
+            def decorator(cls):
+                if not issubclass(cls, Edge):
+                    raise TypeError(f"The class {cls.__name__} must be a subclass of Edge")
+                self.core.process_registry.register(process_name, cls, force=True)
+                return cls
+
+            return decorator
+
+        else:
+
+            # Check if address is a string
+            if isinstance(address, str):
+                protocol, addr = address.split(':', 1)
+                if protocol != 'local':
+                    raise ValueError('BigraphBuilder only supports the local protocol in the current version')
+
+                if addr.startswith('!'):
+                    process_class = local_lookup_module(addr[1:])
+                    self.core.process_registry.register(process_name, process_class, force=True)
+                else:
+                    raise ValueError('Only local addresses starting with "!" are supported')
+
+            # Check if address is a class object
+            elif issubclass(address, Edge):
+                self.core.process_registry.register(process_name, address, force=True)
+            else:
+                raise TypeError(f"Unsupported address type for {process_name}: {type(address)}. Registration failed.")
+
 
 def test_builder():
+    from process_bigraph.experiments.minimal_gillespie import GillespieEvent  # , GillespieInterval
+
     core = ProcessTypes()
 
     initial_tree = {
@@ -200,7 +273,6 @@ def test_builder():
     builder['DNA_store', 'C gene'] = 3.0
     assert builder['DNA_store', 'C gene'].value() == 3.0
 
-
     builder['down', 'here'] = {
         '_value': 10,
         '_type': 'integer'}
@@ -208,7 +280,33 @@ def test_builder():
     x = builder['down', 'here']
     assert x.value() == 10
 
-    builder.visualize(filename='builder_test')
+    # add processes
+    print(f"available processes: {builder.list_processes()}")
+
+    ## register processes by name: what processes do we want and where do they come from
+    builder.register_process(
+        'GillespieEvent', GillespieEvent)
+    builder.register_process(
+        'GillespieInterval',
+        address='local:!process_bigraph.experiments.minimal_gillespie.GillespieInterval')
+
+    ## add processes
+    builder['event_process'].add_process(
+        name='GillespieEvent',
+        kdeg=1.0,  # kwargs fill parameters in the config
+    )
+    builder['interval_process'].add_process(
+        name='GillespieInterval',
+        # inputs={'port_id': ['store']}  # we should be able to set the wires directly like this
+    )
+
+
+
+
+    # make bigraph-viz diagram
+    builder.visualize(filename='builder_test',
+                      show_values=True,
+                      show_types=True)
 
     # make composite, simulate
     composite = builder.generate()
