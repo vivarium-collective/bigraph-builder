135513a7b4846a10a5da360cfedf7465010a14fd
Ryan Spangler <ryan.spangler@gmail.com>
2024-02-27 04:56:13 -0800

    get and set working in a basic way

--- builder/builder_api.py
+++ builder/builder_api.py
@@ -13,23 +13,24 @@ def pf(x):
     return pretty.pformat(x)
 
 
-def builder_tree_from_dict(
+def node_from_tree(
         builder,
         schema,
         tree,
         path=()
 ):
     # TODO -- this might need to use core.fold()
-    builder_tree = BuilderNode(builder, path)
+    node = BuilderNode(builder, path)
     if isinstance(tree, dict):
-        for key, sub_tree in tree.items():
+        for key, subtree in tree.items():
             next_path = path + (key,)
-            builder_tree.branches[key] = builder_tree_from_dict(
+            node.branches[key] = node_from_tree(
                 builder=builder,
                 schema=schema.get(key, schema) if schema else {},
-                tree=sub_tree,
+                tree=subtree,
                 path=next_path)
-    return builder_tree
+
+    return node
 
 
 class BuilderNode:
@@ -39,55 +40,75 @@ class BuilderNode:
         self.path = path
         self.branches = {}
 
+
     def __getitem__(self, keys):
         # Convert single key to tuple
         keys = (keys,) if isinstance(keys, (str, int)) else keys
-        first_key = keys[0]
-        if first_key not in self.branches:
-            self.branches[first_key] = BuilderNode(
+        head = keys[0]
+        if head not in self.branches:
+            self.branches[head] = BuilderNode(
                 builder=self.builder,
-                path=self.path + (first_key,))
+                path=self.path + (head,))
 
-        remaining = keys[1:]
-        if len(remaining) > 0:
-            return self.branches[first_key].__getitem__(remaining)
+        tail = keys[1:]
+        if len(tail) > 0:
+            return self.branches[head].__getitem__(tail)
         else:
-            return self.branches[first_key]
+            return self.branches[head]
+
 
     def __setitem__(self, keys, value):
         # Convert single key to tuple
         keys = (keys,) if isinstance(keys, (str, int)) else keys
-        first_key = keys[0]
-        remaining = keys[1:]
-        path_here = self.path + (first_key,)
+        head = keys[0]
+        tail = keys[1:]
+        path_here = self.path + (head,)
 
-        if first_key not in self.branches:
-            self.branches[first_key] = BuilderNode(
+        if head not in self.branches:
+            self.branches[head] = BuilderNode(
                 builder=self.builder,
                 path=path_here)
 
-        if len(remaining) > 0:
-            self.branches[first_key].__setitem__(remaining, value)
+        if len(tail) > 0:
+            self.branches[head].__setitem__(tail, value)
         elif isinstance(value, dict):
-            # if '_type' in value
+            if '_type' in value:
+                set_path(
+                    tree=self.builder.schema,
+                    path=path_here,
+                    value=value['_type'])
 
-            self.branches[first_key] = builder_tree_from_dict(
-                builder=self.builder,
-                schema=self.get_schema(),
-                tree=value,
-                path=path_here)
+            if '_value' in value:
+                set_path(
+                    tree=self.builder.tree,
+                    path=path_here,
+                    value=value['_value'])
+
+                self.branches[head] = BuilderNode(
+                    builder=self.builder,
+                    path=path_here)
+
+            else:
+                self.branches[head] = node_from_tree(
+                    builder=self.builder,
+                    schema=self.schema(),
+                    tree=value,
+                    path=path_here)
         else:
             # set the value
             set_path(tree=self.builder.tree, path=path_here, value=value)
 
-    def get_tree(self):
+
+    def value(self):
         return get_path(self.builder.tree, self.path)
 
-    def get_schema(self):
+
+    def schema(self):
         return get_path(self.builder.schema, self.path)
 
+
     def top(self):
-        return self.builder.builder_tree
+        return self.builder.node
 
 
 class Builder:
@@ -103,16 +124,16 @@ class Builder:
 
         self.core = core or ProcessTypes()
         self.schema, self.tree = self.core.complete(schema, tree)
-        self.builder_tree = builder_tree_from_dict(self, self.schema, self.tree)
+        self.node = node_from_tree(self, self.schema, self.tree)
 
     def __repr__(self):
         return f"Builder(\n{pf(self.tree)})"
 
     def __getitem__(self, keys):
-        return self.builder_tree[keys]
+        return self.node[keys]
 
     def __setitem__(self, keys, value):
-        self.builder_tree.__setitem__(keys, value)
+        self.node.__setitem__(keys, value)
         self.complete()
 
     def complete(self):
@@ -169,21 +190,30 @@ def test_builder():
             'B rna': 0.0},
     }
 
-    b = Builder(core=core, tree=initial_tree)
+    builder = Builder(core=core, tree=initial_tree)
 
     # test set/get
-    b['down', 'here'] = {'_value': 10, '_type': 'integer'}
-    x = b['down', 'here']
-    # assert x.get_tree() == 10
+    builder['DNA_store', 'C gene'] = 3.0
+    assert builder['DNA_store', 'C gene'].value() == 3.0
 
-    b.visualize(filename='builder_test')
+
+    builder['down', 'here'] = {
+        '_value': 10,
+        '_type': 'integer'}
+
+    import ipdb; ipdb.set_trace()
+
+    x = builder['down', 'here']
+    assert x.value() == 10
+
+    builder.visualize(filename='builder_test')
 
     # make composite, simulate
-    composite = b.generate()
+    composite = builder.generate()
     composite.run(10)
 
     # save document
-    b.write(filename='builder_test_doc')
+    builder.write(filename='builder_test_doc')
 
 
 if __name__ == '__main__':
